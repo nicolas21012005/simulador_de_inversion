f87cebfa649ac4fd16e988dbbe0bfff44f574581
Nicolas Pinzon <np07qsf@gmail.com>
2025-08-28 13:29:57 -0500

    Se adiciona la condicion para no permitir el ingreso de valores nulos en el formulario

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 function simulateInvest() {
-    // let oneYear = document.getElementById('oneYear');
-    // let twoYear = document.getElementById('twoYear');
     let select = document.getElementById('tiempoDeInversion');
     let name = document.getElementById('nombres').value;
     let email = document.getElementById('email').value;
-    // let telefono = document.getElementById('cellPhoneNumber').value;
     let monto = parseFloat(document.getElementById('monto').value);
     let mostrarNombres = document.getElementById('spanNombre');
     let totalInversionPerson = document.getElementById('totalInversion');
@@ -14,6 +11,7 @@ function simulateInvest() {
     let showDivPre = document.querySelector('.pre-simulation');
     let porcentaje = document.getElementById('porcentaje');
     let meses = document.getElementById('meses');
+    let telefono = document.getElementById('cellPhoneNumber');
     showDiv.classList.remove("disabled");
     showDivPre.classList.add("disabled");
 
@@ -21,32 +19,37 @@ function simulateInvest() {
     mostrarNombres.textContent = name;
     let totalInversion = 0;
     let ganancia = 0;
-    switch (select.value) {
-        case 'oneYear':
-            porcentaje.textContent = "0.8%";
-            meses.textContent = "12 meses";
-            totalInversion = (monto * 0.008) * 12 + monto
-            ganancia = (monto * 0.008) * 12
-            break;
-        case 'twoYear':
-            porcentaje.textContent = "1.3%";
-            meses.textContent = "24 meses";
-            totalInversion = (monto * 0.013) * 24 + monto
-            ganancia = (monto * 0.013) * 24
-            break;
-        case 'threeYear':
-            porcentaje.textContent = "1.7%";
-            meses.textContent = "36 meses";
-            totalInversion = (monto * 0.017) * 36 + monto
-            ganancia = (monto * 0.017) * 36
-            break;
 
-        default:
-            break;
+    if (name == '' || email == '' || telefono == '' || monto <= 0 || select.value === '') {
+        alert("Por favor, complete todos los campos correctamente.");
+        return;
+    } else {
+        switch (select.value) {
+            case 'oneYear':
+                porcentaje.textContent = "0.8%";
+                meses.textContent = "12 meses";
+                totalInversion = (monto * 0.008) * 12 + monto
+                ganancia = (monto * 0.008) * 12
+                break;
+            case 'twoYear':
+                porcentaje.textContent = "1.3%";
+                meses.textContent = "24 meses";
+                totalInversion = (monto * 0.013) * 24 + monto
+                ganancia = (monto * 0.013) * 24
+                break;
+            case 'threeYear':
+                porcentaje.textContent = "1.7%";
+                meses.textContent = "36 meses";
+                totalInversion = (monto * 0.017) * 36 + monto
+                ganancia = (monto * 0.017) * 36
+                break;
+
+            default:
+                break;
+        }
+        totalInversionPerson.textContent = totalInversion.toFixed(2);
+        gananciaPerson.textContent = ganancia.toFixed(2);
     }
-
-    totalInversionPerson.textContent = totalInversion.toFixed(2);
-    gananciaPerson.textContent = ganancia.toFixed(2);
 }
 
 
